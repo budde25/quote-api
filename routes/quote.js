da97dc10bf4d875523b9b27619be9dceff845d01
Ethan Budd <ethan@ebudd.io>
2020-10-14 12:34:33 -0500

    updates deps

--- routes/quote.js
+++ routes/quote.js
@@ -1,11 +1,15 @@
-//import * as tf from '@tensorflow/tfjs-node';
-
 var express = require('express');
+const tf = require("@tensorflow/tfjs");
+require("@tensorflow/tfjs-node");
 var router = express.Router();
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  const mod = tf.models.modelFromJSON('/model.json');
+  const model = tf.sequential();
+
+
   res.send('respond with a resource');
 });
 
